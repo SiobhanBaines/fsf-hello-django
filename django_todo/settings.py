3e5bf34205c090dba553caef2df6e3c7dcb45640
SiobhanBaines <siobhan.baines@gmail.com>
2021-05-11 14:57:32 +0000

    Missing bracket on secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-1h91e^q=2xed6@6x+5legh@1dr2iu%yzwu#+@ind+gi9d!_ud%'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-1h91e^q=2xed6@6x+5legh@1dr2iu%yzwu#+@ind+gi9d!_ud%')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
